1f77f076e4afec43653bb0c8ef7bcd7db31e9516
Ashwin <ashwin.chandrasekaran@tretaillabs.com>
2023-08-18 12:29:12 +0800

    Initial

---
+++ simpleunittest/calculations.py
@@ -0,0 +1,13 @@
+class Calculations:
+
+    def sum(self, a, b):
+        return a + b
+    
+    def multiply(self, a, b):
+        return a * b
+    
+    def subtract(self, a, b):
+        return a - b
+    
+    def divide(self, a, b):
+        return a / b
